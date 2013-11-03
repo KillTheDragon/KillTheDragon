2399fcab2a2a13f2e17da1e4547c8a304f05af61
Alex Dickson <winklenurfer@gmail.com>
2013-11-03 15:48:43 -0800

    adding render for index.html

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ http.createServer(function (req, res) {
         }
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("Welcome to Node.js on OpenShift!\n\n");
-  res.end("Your IP address seems to be " + addr + "\n");
+  res.render('index.html');
 }).listen(port, ipaddr);
 console.log("Server running at http://" + ipaddr + ":" + port + "/");
