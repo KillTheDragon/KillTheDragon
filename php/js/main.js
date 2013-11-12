81d1a55e911582b2eb0d3a0f025be57ad3f7d384
winklenurfer@gmail.com <alex.dickson@blackboard.com>
2013-11-11 19:08:20 -0800

    adding health font scaling

--- php/js/main.js
+++ php/js/main.js
@@ -6,9 +6,30 @@ $(document).ready(function() {
     var dragon_health_curr = 100;
     
     var update_health = function() {
-        $(".player_health").html("<h3>You: " + player_health_curr + "/" + player_health_max + "</h3>");
-        $(".dragon_health").html("<h2>Dragon: " + dragon_health_curr + "/" + dragon_health_max + "</h2>");
-    }
+        
+        var player_font = function() {
+            if (player_health_curr / 2 < 15) {
+                return 15;
+            } else if (player_health_curr / 2 > 50) {
+                return 50;
+            } else {
+                return player_health_curr / 2;
+            }
+        };
+        
+        var dragon_font = function() {
+            if (dragon_health_curr / 2 < 15) {
+                return 15;
+            } else if (dragon_health_curr / 2 > 50) {
+                return 50;
+            } else {
+                return dragon_health_curr / 2;
+            }
+        };
+    
+        $(".player_health").html("<h3 style=\"font-size:"+ player_font() + "px\">You: " + player_health_curr + "/" + player_health_max + "</h3>");
+        $(".dragon_health").html("<h3 style=\"font-size:"+ dragon_font() + "px\">Dragon: " + dragon_health_curr + "/" + dragon_health_max + "</h3>");
+    };
     
     update_health();
     
